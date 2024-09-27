ae2dec2b108210c5a5990a04c6a0787086efb17d
Rishavdovakiin <brishav369gmail.com>
2024-09-28 01:53:32 +0530

    modified API_Modified

--- API_Modified.py
+++ API_Modified.py
@@ -33,6 +33,17 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+import os
+
+app = FastAPI()
+
+# Get the port from the environment variable or default to 8000
+port = int(os.getenv("PORT", 10000))
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
 # Define the DataFrameSelector class for selecting columns
 from sklearn.base import BaseEstimator, TransformerMixin
 
@@ -263,13 +274,4 @@ async def download_plot():
         return FileResponse(plot_path, media_type="image/png", filename="predictions_plot.png")
     raise HTTPException(status_code=404, detail="Plot file not found")
 
-import os
 
-app = FastAPI()
-
-# Get the port from the environment variable or default to 8000
-port = int(os.getenv("PORT", 8000))
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=port)
