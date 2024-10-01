3f92aa1bea2e3c90759f7e315225b40890a58e82
Rishavdovakiin <brishav369gmail.com>
2024-10-02 00:06:49 +0530

    modified streamlit_frontend.py

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -16,7 +16,7 @@ API_URL = "https://dark-matter-halo-concentration.onrender.com/"  # Update this 
 page_bg_img = '''
 <style>
 .stApp {
-    background-image: url("https://github.com/Rishavdovakiin/DM-Regression/blob/main/img.png");
+    background-image: url("https://i.pinimg.com/564x/20/43/54/204354a9dee9d5e126cc58561a8b5c43.jpg");
     background-size: cover;
     background-position: center;
 }
